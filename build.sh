70166fbf06a17ef4d1e5a3b78b3152337200a3c1
midoyasser16204 <hamada16204@gmail.com>
2026-06-19 17:26:41 +0300

    update build

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing dependencies..."
+echo "Installing Flutter (includes Dart)..."
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$HOME/flutter/bin:$PATH"
+
+flutter --version
+dart --version
+
+echo "Getting dependencies..."
 dart pub get
 
-echo "Running Jaspr build via build_runner..."
+echo "Building Jaspr..."
 dart run build_runner build -d
 
 echo "Build complete."
